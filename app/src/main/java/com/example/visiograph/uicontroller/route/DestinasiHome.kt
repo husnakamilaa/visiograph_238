2893a74e123a13a5a43e81a899fcc5cbf8205fcd
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2026-01-15 18:18:00 +0700

    feat: menambahkan destinasi home atau dashboard

---
+++ app/src/main/java/com/example/visiograph/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.visiograph.uicontroller.route
+
+import com.example.visiograph.R
+
+object DestinasiHome: DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
